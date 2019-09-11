30a765cb4f3739d8186ceece5009185f2c970d26
Renette Ros <supergirl0894+git@gmail.com>
2019-09-11 14:28:11 +0200

    Reduce logging during unit tests

--- cloud-stream-demo/sink/src/test/kotlin/com/example/cloudstream/demo/SinkDemoApplicationTest.kt
+++ cloud-stream-demo/sink/src/test/kotlin/com/example/cloudstream/demo/SinkDemoApplicationTest.kt
@@ -3,12 +3,10 @@ package com.example.cloudstream.demo
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-@ActiveProfiles("local")
 class SinkDemoApplicationTest {
 
     @Test
