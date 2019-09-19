03252afe66dbb20f8f23434470a83e2c2d8e7609
Renette Ros <supergirl0894+git@gmail.com>
2019-09-19 14:32:49 +0200

    Change response status for message publication to 201 Created

--- cloud-stream-demo/web-source/src/main/kotlin/com/example/cloudstream/demo/MessagePublishingController.kt
+++ cloud-stream-demo/web-source/src/main/kotlin/com/example/cloudstream/demo/MessagePublishingController.kt
@@ -1,16 +1,14 @@
 package com.example.cloudstream.demo
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class MessagePublishingController(val messagePublisher: MessagePublisher,
                                   val testProperties: TestProperties) {
 
     @PostMapping("publish")
+    @ResponseStatus(HttpStatus.CREATED)
     fun sendMessage(@RequestBody message: String) {
         messagePublisher.publishMessage(message)
     }
