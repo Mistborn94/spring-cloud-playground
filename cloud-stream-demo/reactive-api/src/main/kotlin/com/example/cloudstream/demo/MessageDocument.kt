d76ffd4072d81095aaadaaa6716bf5adac9528b6
Renette Ros <supergirl0894+git@gmail.com>
2019-09-19 09:53:11 +0200

    Change reactive api to also use instant and serialize it correctly.

--- cloud-stream-demo/reactive-api/src/main/kotlin/com/example/cloudstream/demo/MessageDocument.kt
+++ cloud-stream-demo/reactive-api/src/main/kotlin/com/example/cloudstream/demo/MessageDocument.kt
@@ -1,12 +1,15 @@
 package com.example.cloudstream.demo
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import java.time.LocalDateTime
+import java.time.Instant
 
 @Document("messages")
 data class MessageDocument constructor(@Id val id: String,
                                        val content: String,
-                                       //The local date time fetches the date from the database and converts it to a
-                                       //LocalDateTime in the system default timezone. It works fine for display purposes.
-                                       val timestamp: LocalDateTime)
+                                        //Setting only the pattern causes failures. Setting only the timezone does not work as expected.
+                                        // An alternative option is using LocalDateTime since it is in the system default timezone.
+                                        // LocalDateTime works okay for display purposes, but does not show the timezone
+                                       @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX", timezone = "CAT") val timestamp: Instant)
